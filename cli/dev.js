a6c668a4d53b40a2f9ddbfd00dc4ef60b8249a76
ivanq3w <ivanq3w@gmail.com>
2021-09-23 18:39:54 +0300

    NPM publish wip

---
+++ cli/dev.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+console.log('console.log output')
+const path = require('path')
+const fs = require('fs')
+console.log('__dirname', __dirname)
+console.log('process.cwd()', process.cwd())
